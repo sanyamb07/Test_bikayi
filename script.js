81d5a417f3c5054f25713da21821d9fcd3b66ef1
sdsd <sahaniv66@gmail.com>
2020-01-03 14:33:39 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function createTable(data){
+var set = new Set();
+function createTable(data,firstTime = false){
     $('#jsonTable').html("");
     $('#jsonTable').append(`<th>Year</th>
     <th>Category</th>
@@ -6,61 +7,52 @@ function createTable(data){
     <th>Laureates</th>
     <th>Laureates</th>`);
     $.each( data["prizes"], function(key,val ) {
-
-        $('#jsonTable').css("visibility", "visible").css("border", "1px solid green");
+        $('#jsonTable').css("visibility", "visible");
         $('#jsonTable').append("<tbody>");
         $('#jsonTable').append("<td>"+val.year+"</td>"+"<td>" +val.category+"</td>");
-        $('#sel').append('<option value="' + val.year + '">' + val.year + '</option>');
-        $('#sel').append('<option value="' + val.category + '">' + val.category + '</option>');
-
+        if(!$(`#${val.year}`).length)
+        $('#sel').append(`<option value="year" id="${val.year}"> ${val.year} </option>`);
+        if(!$(`#${val.category}`).length)
+        $('#select').append(`<option value="category" id="${val.category}">${val.category}</option>`);  
         if (val["laureates"]){
-            $.each(val["laureates"],function(k,v){ 
-                
+            $.each(val["laureates"],function(k,v){
                 $('#jsonTable').append("<td>"+ v.firstname+" "+v.surname+"</td>");
-                                
-    
+                if(firstTime){
+                    if(set.has(v.id)){
+                        if(v.surname != undefined )
+                        $("#repeating-names").append(`<p>${v.firstname + v.surname}</p>`)
+                    }else{
+                        set.add(v.id);
+                    }
+                }                
             });       
         }else{
             return;
         }
-
         $('#jsonTable').append("</tbody>");
-        $('th').css("border", "1px solid green");
-    
     });
 }
-$(document).ready(function(){
-   
-    $.getJSON( "https://api.nobelprize.org/v1/prize.json", function( data ) {
-        createTable(data);
-        $('#sel').change(function () {
-            var year = $('#sel').find(":selected").text();
 
+$(document).ready(function(){
+    $.getJSON( "https://api.nobelprize.org/v1/prize.json", function( data ) {
+        createTable(data,true);
+        $('select').change(function () {
+           
+            year = $('#sel').find(":selected").text();
+            category =$('#select').find(":selected").text();
             $.ajax({
                 url         : 'https://api.nobelprize.org/v1/prize.json',
-                data        : {"year":year},
+                data        : {"year":year,"category":category},
                 dataType    : "json",
                 cache       : false,
                 contentType : 'application/json; charset=utf-8',
                 type        : 'GET',
-                success     : function(data){ // this is where you do something with response
-                    console.log(data);
+                success     : function(data){
+                     // this is where you do something with response
                     createTable(data);
-                   // The below is my assumption like filter the trs which do not contain 
-                   // 2015
-                   
-                //    $('#jsonTable tbody').find('td').filter(function(td){
-                //       $(tr).find('td').last().text().trim() !== year;
-                //    }).hide();
+                  
                 }
-           });
-            
+           }); 
         });
-
     }); 
-    
-    
-
 });
-
-  
